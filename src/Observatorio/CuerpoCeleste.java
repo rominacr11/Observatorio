2ca7277812093bdda55089f9fbedc08beb5f1752
Romina Cruz Rosas <rominacr458@gmail.com>
2026-04-15 01:23:45 -0600

    nueva version

--- src/Observatorio/CuerpoCeleste.java
+++ src/Observatorio/CuerpoCeleste.java
@@ -1,13 +1,17 @@
 package Observatorio;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
-public class CuerpoCeleste {
+import java.util.Objects;
+
+public class CuerpoCeleste implements Comparable<CuerpoCeleste> {
 
     private String nombre;
     private String composicion;
     private Double distanciaDeLaTierra;
     private String unidadDeLaDistancia;
     private List<Observacion> observaciones;
+
 
     public CuerpoCeleste(String nombre, String composicion, Double DistanciaDeLaTierra, String UnidadDeLaDistancia) {
         this.nombre = nombre;
@@ -19,6 +23,7 @@ public class CuerpoCeleste {
     public void agregarObservacion(Observacion observacion) {
         observaciones.add(observacion);
     }
+
 
     public Double getDistanciaDeLaTierra() {
         return distanciaDeLaTierra;
@@ -40,5 +45,42 @@ public class CuerpoCeleste {
         ", composición= " + composicion +
                 ", distancia: " + distanciaDeLaTierra + " " + unidadDeLaDistancia +
                 "}";
+
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) return false;
+        CuerpoCeleste that = (CuerpoCeleste) o;
+        return Objects.equals(nombre, that.nombre) &&
+                Objects.equals(composicion, that.composicion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, composicion);
+    }
+
+
+    @Override
+    public int compareTo(CuerpoCeleste otro) {
+        if (this.distanciaDeLaTierra == null && otro.distanciaDeLaTierra == null) return 0;// no hay distancias
+        if (this.distanciaDeLaTierra == null) return 1;  // un objeto no tiene distancia pero el que sigue si, y va al final
+        if (otro.distanciaDeLaTierra == null) return -1;// va el objeto con distancia al principio
+        return this.distanciaDeLaTierra.compareTo(otro.distanciaDeLaTierra);//misma distancia
+
+    }
+
+
+    public static Comparator<CuerpoCeleste> comparadorPorNombre = new Comparator<CuerpoCeleste>() {
+        @Override
+        public int compare(CuerpoCeleste c1, CuerpoCeleste c2) {
+            return c1.nombre.compareTo(c2.nombre);
+        }
+    };
+
 }
+
--- src/Observatorio/Observacion.java
+++ src/Observatorio/Observacion.java
@@ -1,11 +1,17 @@
 package Observatorio;
-public class Observacion {
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Observacion implements Comparable<Observacion> {
+
     private Double latitud;
     private String latitudHemisferio;
     private Double longitud;
     private String longitudHemisferio;
     private String mesDeObservacion;
     private Double desplazamiento;
+
     public Observacion(Double latitud, String latitudHemisferio, Double longitud, String longitudHemisferio,String mesDeObservacion) {
         this.latitud = latitud;
         this.latitudHemisferio = latitudHemisferio;
@@ -35,8 +41,39 @@ public class Observacion {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Observacion that = (Observacion) o;
+        return Objects.equals(latitud, that.latitud)
+                && Objects.equals(latitudHemisferio, that.latitudHemisferio)
+                && Objects.equals(longitud, that.longitud)
+                && Objects.equals(longitudHemisferio, that.longitudHemisferio)
+                && Objects.equals(mesDeObservacion, that.mesDeObservacion)
+                && Objects.equals(desplazamiento, that.desplazamiento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitud, latitudHemisferio, longitud, longitudHemisferio, mesDeObservacion, desplazamiento);
+    }
+
+    @Override
     public String toString() {
         return "observación: " + obtenerPosicion() + " en " + mesDeObservacion;
+    }
+    @Override
+    public int compareTo(Observacion otra) {
+        if (this.latitud == null && otra.latitud == null) return 0;
+        if (this.latitud == null) return 1;   // los nulos al final
+        if (otra.latitud == null) return -1;
+        return this.latitud.compareTo(otra.latitud);
 
     }
+    public static Comparator<Observacion> comparadorPorMes = new Comparator<Observacion>() {
+        @Override
+        public int compare(Observacion o1, Observacion o2) {
+            return Objects.compare(o1.mesDeObservacion, o2.mesDeObservacion, String::compareTo);
+        }
+
+    };
 }
